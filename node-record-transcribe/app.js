bf4efe601250ba923280eb72f0a1aa27ed42dd3e
PrasannaP <mail2lulu@gmail.com>
2023-05-31 11:48:31 +0530

    This is working one

---
+++ node-record-transcribe/app.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const ffmpeg = require("fluent-ffmpeg");
+const mic = require("mic");
+const { Readable } = require("stream");
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+// Record audio
+function recordAudio(filename) {
+    return new Promise((resolve, reject) => {
+        const micInstance = mic({
+            rate: "16000",
+            channels: "1",
+            fileType: "wav",
+        });
+
+        const micInputStream = micInstance.getAudioStream();
+        const output = fs.createWriteStream(filename);
+        const writable = new Readable().wrap(micInputStream);
+
+        console.log("Recording... Press Ctrl+C to stop.");
+
+        writable.pipe(output);
+
+        micInstance.start();
+
+        process.on("SIGINT", () => {
+            micInstance.stop();
+            console.log("Finished recording");
+            resolve();
+        });
+
+        micInputStream.on("error", (err) => {
+            reject(err);
+        });
+    });
+}
+
+// Transcribe audio
+async function transcribeAudio(filename) {
+    const transcript = await openai.createTranscription(
+        fs.createReadStream(filename),
+        "whisper-1"
+    );
+    return transcript.data.text;
+}
+
+// Main function
+async function main() {
+    const audioFilename = "recorded_audio.wav";
+    await recordAudio(audioFilename);
+    const transcription = await transcribeAudio(audioFilename);
+    console.log("Transcription:", transcription);
+}
+
+main();
---
+++ readme.md
@@ -0,0 +1,15 @@
+## ref and commands
+
+https://medium.com/codingthesmartway-com-blog/speech-to-text-use-node-js-and-openais-whisper-api-to-record-transcribe-in-one-step-c9a1fd441765
+
+
+
+mkdir node-record-transcribe
+cd node-record-transscribe
+npm init -y
+touch app.js
+npm install openai @ffmpeg-installer/ffmpeg fluent-ffmpeg mic
+
+node app.js
+
+## This is working one
